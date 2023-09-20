74297f2051910e5f0d1f3e6a90db9adb4b6e98bd
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-20 16:54:38 +0200

    styling

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,7 @@
-import React, { useContext } from "react";
-import "../scss/pages/projects.scss";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 import Language from "../components/parts/Language";
 import Carousel from "../components/Carousel";
-import { ThemeContext } from "../context/ThemeContext";
 
 function Projects() {
   const { darkMode } = useContext(ThemeContext);
@@ -36,12 +35,19 @@ function Projects() {
         >
           <div className="h3-text-project-one">
             <h3 className={darkMode ? "dark-text m-font" : "light-text m-font"}>
-              <Language englishText={`ABOUT US`} polishText={`O NAS`} />
+              <Language
+                englishText={`PLASTERING`}
+                polishText={`PLASTROWANIE`}
+              />
             </h3>
           </div>
           <div className="p-text-project-one">
             <p className={darkMode ? "dark-text s-font" : "light-text s-font"}>
-              <Language englishText={`ABOUT US`} polishText={`O NAS`} />
+              <Language
+                englishText={`Plastering is the foundation of a strong and visually appealing finish in construction and renovation. Our experienced team excels in this art, using high-quality materials and modern techniques to ensure durability and beauty. Whether it's repairing walls, preparing surfaces for paint, or enhancing interiors, we provide precision and professionalism. With a focus on both new construction and renovations, we transform walls and ceilings, creating lasting and elegant spaces that reflect your unique style and needs`}
+                polishText={`Plastrowanie to fundament trwałego i estetycznego wykończenia w budownictwie i renowacji. Nasz doświadczony zespół doskonale się w tym sprawdza, używając wysokiej jakości materiałów i nowoczesnych technik, zapewniając trwałość i piękno. Czy to naprawa ścian, przygotowanie powierzchni do malowania czy też poprawa wnętrz, zapewniamy precyzję i profesjonalizm. Skupiając się zarówno na nowych budowlach, jak i renowacjach, przekształcamy ściany i sufity, tworząc trwałe i eleganckie przestrzenie, które odzwierciedlają Twój wyjątkowy styl i potrzeby.
+                `}
+              />
             </p>
           </div>
         </div>
@@ -54,12 +60,15 @@ function Projects() {
         >
           <div className="h3-text-project-two">
             <h3 className={darkMode ? "dark-text m-font" : "light-text m-font"}>
-              <Language englishText={`ABOUT US`} polishText={`O NAS`} />
+              <Language englishText={`PAINTING`} polishText={`MALOWANIE`} />
             </h3>
           </div>
           <div className="p-text-project-two">
             <p className={darkMode ? "dark-text s-font" : "light-text s-font"}>
-              <Language englishText={`ABOUT US`} polishText={`O NAS`} />
+              <Language
+                englishText={`Painting is the art of adding color and life to your spaces. Our skilled painters bring expertise and creativity to every project. Whether it's refreshing a room's appearance, creating stunning decorative finishes, or restoring the beauty of your walls, we deliver outstanding results. We take pride in transforming your spaces with precision and flair.`}
+                polishText={`Malowanie to sztuka dodawania koloru i życia Twoim przestrzeniom. Nasi wykwalifikowani malarze przynoszą do każdego projektu swoje umiejętności i kreatywność. Czy to odświeżanie wyglądu pomieszczeń, tworzenie zachwycających efektów dekoracyjnych czy też przywracanie piękna Twoim ścianom, dostarczamy wyjątkowych rezultatów. Jesteśmy dumni z przekształcania Twoich przestrzeni z precyzją i fantazją.`}
+              />
             </p>
           </div>
         </div>
@@ -78,12 +87,15 @@ function Projects() {
         >
           <div className="h3-text-project-three">
             <h3 className={darkMode ? "dark-text m-font" : "light-text m-font"}>
-              <Language englishText={`ABOUT US`} polishText={`O NAS`} />
+              <Language englishText={`RENOVATIONS`} polishText={`O NAS`} />
             </h3>
           </div>
           <div className="p-text-project-three">
             <p className={darkMode ? "dark-text s-font" : "light-text s-font"}>
-              <Language englishText={`ABOUT US`} polishText={`O NAS`} />
+              <Language
+                englishText={`Renovations breathe new life into your spaces, and our expert team is dedicated to revitalizing your surroundings. From comprehensive remodeling projects to meticulous restorations, we bring a wealth of experience and craftsmanship to every job. Whether it's transforming a dated interior, enhancing functionality, or preserving the historical charm of a property, we take pride in delivering exceptional renovation solutions that meet your unique needs and preferences.`}
+                polishText={`Remonty ożywiają Twoje przestrzenie, a nasz doświadczony zespół jest oddany ożywianiu Twojego otoczenia. Od kompleksowych projektów remontowych po staranne renowacje, przynosimy ogromne doświadczenie i rzemieślniczą precyzję do każdej pracy. Czy to transformacja przestarzałego wnętrza, zwiększanie funkcjonalności czy zachowywanie historycznego uroku nieruchomości, jesteśmy dumni z dostarczania wyjątkowych rozwiązań remontowych, które spełniają Twoje unikalne potrzeby i preferencje.`}
+              />
             </p>
           </div>
         </div>
