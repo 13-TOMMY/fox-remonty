e9d657556e00f6b5eabadbe604be3922ba93ebeb
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-26 20:37:21 +0200

    test

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,8 +7,7 @@ function Banner() {
       <img src="/imgs/RE.svg" alt="" />
       <div className="banner-scrolling-text" >
         <h2 className="banner-scrolling-text-content">
-          FOX REMONTY FOX REMONTY
-        </h2>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta esse cumque repellat? Harum exercitationem fuga totam eveniet quo ex, aut voluptas provident mollitia, illo error alias recusandae molestiae quae enim?        </h2>
       </div>
     </div>
   );
