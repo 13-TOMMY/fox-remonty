92ac407e5e2b61af35a0f2b36348d5d8621ea245
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-26 19:37:31 +0200

    test and gasp added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,7 +7,7 @@ function Banner() {
       <img src="/imgs/RE.svg" alt="" />
       <div className="banner-scrolling-text" >
         <h2 className="banner-scrolling-text-content">
-          FOX REMONTY
+          FOX REMONTY FOX REMONTY
         </h2>
       </div>
     </div>
