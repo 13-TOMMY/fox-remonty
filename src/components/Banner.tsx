03554b5fd8badc2a33d59a8f690a92f9b5174fde
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-26 21:56:04 +0200

    test

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,11 @@
-
-
 function Banner() {
-  
   return (
-    <div className="banner-container" >
+    <div className="banner-container">
       <img src="/imgs/RE.svg" alt="" />
-      <div className="banner-scrolling-text" >
+      <div className="banner-scrolling-text">
         <h2 className="banner-scrolling-text-content">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta esse cumque repellat? Harum exercitationem fuga totam eveniet quo ex, aut voluptas provident mollitia, illo error alias recusandae molestiae quae enim?        </h2>
+         otottoototototototot
+        </h2>
       </div>
     </div>
   );
