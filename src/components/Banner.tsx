1bc606bcb3131a257a9e236f8f588807549c2deb
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-27 10:23:23 +0200

    added a marquee

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,13 @@
+
 function Banner() {
+
+  
+
   return (
     <div className="banner-container">
       <img src="/imgs/RE.svg" alt="" />
       <div className="banner-scrolling-text">
-        <h2 className="banner-scrolling-text-content">
-         otottoototototototot
-        </h2>
+        
       </div>
     </div>
   );
--- src/components/Keywords.tsx
+++ src/components/Keywords.tsx
@@ -1,28 +1,30 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-import { motion, useScroll, useTransform } from "framer-motion";
 import Language from "./parts/Language";
 
 function Keywords() {
   const { darkMode } = useContext(ThemeContext);
-  const { scrollY } = useScroll();
-  const x = useTransform(scrollY, [0, 0.5, 1], ["-50%", "0%", "50%"]);
+
   return (
-    <div className={"scroll-container"}>
-      <motion.h3
-        className={darkMode ? "dark-text" : "light-text"}
-        style={{ x }}
-        initial={{ x: "-50%" }}
-      >
-        <Language
-          englishText={
-            "Reliable Quick Affordable Fast Professional Experience Quality Affordable Trusted"
+    <div className="marquee-container">
+      <div className="marquee">
+        <div
+          className={
+            darkMode
+              ? "dark-marquee marquee-content scroll"
+              : "light-marquee marquee-content scroll"
           }
-          polishText={
-            "Wiarygodny Szybko Tanio Efektywny Profesjonalny Doświadczony Wysoka jakość Przystępny cenowo Godny zaufania"
-          }
-        />
-      </motion.h3>
+        >
+          <Language
+            englishText={
+              "Reliable | Quick | Affordable | Fast | Professional | Experience | Quality | Affordable | Trusted"
+            }
+            polishText={
+              "Wiarygodny | Szybko | Tanio | Efektywny | Profesjonalny | Doświadczony | Wysoka jakość | Przystępny cenowo | Godny zaufania"
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 }
