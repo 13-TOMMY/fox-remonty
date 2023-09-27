626a8a0b7a367abad8b4bcc98c2911cc4664f744
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-27 20:40:42 +0200

    added banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,16 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 function Banner() {
-
-  
-
+  const {darkMode} = useContext(ThemeContext)
   return (
     <div className="banner-container">
-      <img src="/imgs/RE.svg" alt="" />
-      <div className="banner-scrolling-text">
-        
-      </div>
+      <img
+        src={darkMode ? "/imgs/fox-remonty-banner.png" : "/imgs/fox-remonty-banner-light.png"}
+        alt="fox-remonty-logo"
+        className="banner-text"
+      />
+      <img src="/imgs/RE.svg" alt="fox-remonty-logo" className="banner-img"/>
     </div>
   );
 }
