2910b747680dd6ed8ce79192a279b4d0f07e8033
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-09-27 21:13:51 +0200

    changed postion of banner elments, changed size of bottom homepage logo

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,12 +5,12 @@ function Banner() {
   const {darkMode} = useContext(ThemeContext)
   return (
     <div className="banner-container">
+      <img src="/imgs/RE.svg" alt="fox-remonty-logo" className="banner-img"/>
       <img
         src={darkMode ? "/imgs/fox-remonty-banner.png" : "/imgs/fox-remonty-banner-light.png"}
         alt="fox-remonty-logo"
         className="banner-text"
       />
-      <img src="/imgs/RE.svg" alt="fox-remonty-logo" className="banner-img"/>
     </div>
   );
 }
